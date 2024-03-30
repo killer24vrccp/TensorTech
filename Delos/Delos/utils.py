d77037040b17f27d4215f62a7ca7f81510ddc6f9
killercedou24 <cedric.parent@tensortechlab.com>
2024-03-30 01:55:42 -0400

    Create Django project file structure and add .gitignore
    This commit creates a Django project called 'Delos' and sets up the basic file structure with .gitignore file. This includes settings, asgi, and wsgi configuration files. A requirements.txt file listing project dependencies and a utility script for system information are also added. Modifications in the README file detail the default packages used in the project.

---
+++ Delos/Delos/utils.py
@@ -0,0 +1,20 @@
+import pprint
+
+import psutil
+import platform
+
+OS = platform.system()  # Display ex: Linux, Windows
+RELEASE = platform.release()  # Display ex: 11
+ARCH = platform.architecture()[0]  # Display ex: x64 or x86
+PROCESSOR_NAME = platform.processor()  # Display ex: Intel64 Family 6 Model 183 Stepping 1
+PYTHON_VERSION = platform.python_version()  # Display ex: 3.11.2
+
+CORE_CPU = psutil.cpu_count(logical=False)  # Display only cpu core physical
+CORE_THREADS = psutil.cpu_count(logical=True)  # Display with thread logical
+CPU_FREQUENCY = psutil.cpu_freq()[0]  # Display CPU Frequency clock
+CPU_USAGE = f'{psutil.cpu_percent()}%'  # Display CPU usage in %
+DISK_USAGE = psutil.disk_usage('C:/' or '/')  # Display Free space in bytes, Usage space in byte and Free space in %
+USER = psutil.users()[0][0]  # Display root user where application run
+MEMORY_USAGE = f'{psutil.virtual_memory()[2]}%'  # Display Free Memory in %
+
+print(CPU_USAGE)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TensorTech
 TensorTech Project
+
+
+## Default package 
+
+1. django
+2. django-environ
+3. django-bootstrap-v5
