2a8e630b18a298d0ec561719b2ae778df68a6794
killer24vrccp <cedric.parent@tensortechlab.com>
2024-04-03 01:25:45 -0400

    Refactor authentication views to simplify tenant-team relationship
    Authentication views have been streamlined for simplicity and improved readability. Now, the system checks whether a user has a team and that team has a tenant in a single step. Additionally, the 'Domain' related code has been optimized - directly fetching the domain using the tenant ID associated with the user's team.

--- authentication/views.py
+++ authentication/views.py
@@ -24,35 +24,13 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
 
-                # Check if the user is associated with a team
-                if user.team is None:
-                    messages.add_message(request, messages.ERROR, "You do not belong to any team.")
+                if not user.team or not user.team.tenant:
                     return render(request, self.template_name, {'form': form})
-
-                team = user.team
-
-                # Check if the team has a tenant associated with it
-                if team.tenant is None:
-                    messages.add_message(request, messages.ERROR, "Your team doesn't have a tenant associated.")
-                    return render(request, self.template_name, {'form': form})
-
-                tenant = team.tenant
 
                 try:
-                    domain = Domain.objects.get(tenant=tenant)
-                    tenant_domain = domain.domain
+                    domain = Domain.objects.get(tenant=user.team.tenant.id)
                 except ObjectDoesNotExist:
-                    messages.add_message(request, messages.ERROR, "No Domain associated with your Tenant.")
                     return render(request, self.template_name, {'form': form})
 
-                # Build the redirection URL
-                domain_url = request.scheme + '://' + tenant_domain
-
-                # Redirection vers le domaine du locataire
-                return redirect(domain_url)
-            else:
-                # Retourner un message d'erreur de connexion invalide
-                return render(request, self.template_name,
-                              {'form': form, 'error_message': 'Invalid email or password.'})
-        else:
-            return render(request, self.template_name, {'form': form})
+                return redirect(request.scheme + '://' + domain.domain)
+        return render(request, self.template_name, {'form': form})
